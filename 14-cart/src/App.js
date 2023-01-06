25bf682a7937277cf07cb9720839b520d6aff88d
Rodwyn Moreno <rodwynmoreno@gmail.com>
2023-01-06 17:42:13 -0600

    Added reducer exercise.

---
+++ 14-cart/src/App.js
@@ -0,0 +1,22 @@
+import { CartContainer } from './CartContainer';
+import { useGlobalContext } from './context'
+import { Navbar } from './Navbar';
+
+export const App = () => {
+  const { loading } = useGlobalContext();
+
+  if (loading) {
+    return (
+      <div className='loading'>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
